c5ba6162952aa6d032613f845ea6ddfea635ab35
Kenneth Jones <admin@techhandie.com>
2020-07-30 21:57:38 -0400

    l84 before div.bloc

--- index.js
+++ index.js
@@ -81,6 +81,6 @@ function addFleurDeLis() {
 
 $("h1").addClass("title");
 $(".title").before('<h1 class="open">We Greet You with</h1>');
-$("a").before('<h2 class="close">and your response is</h2>');
+$("div.bloc").before('<h2 class="close">and your response is</h2>');
 
 addFleurDeLis();
