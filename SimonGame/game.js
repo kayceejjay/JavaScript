b8553183a1ffd08fb364f5c05a53e6d273e3d50e
Kenneth Jones <admin@techhandie.com>
2020-08-02 17:01:30 -0400

    update game.js

--- SimonGame/game.js
+++ SimonGame/game.js
@@ -2,33 +2,25 @@ var gamePattern = [];
 var buttonColors = ["red","blue","green","yellow"];
 
 function playSound(color) {
-
   var sounds = ["red.mp3","blue.mp3","green.mp3","yellow.mp3","wrong.mp3"];
-
   switch (color) {
     case "red":
       sound = sounds[0];
-
       break;
     case "blue":
       sound = sounds[1];
-
       break;
     case "green":
       sound = sounds[2];
-
       break;
     case "yellow":
       sound = sounds[3];
-
       break;
     default:
       sound = sound[4];
   }//switch
-
   var soundToPlay = new Audio(sound);
   soundToPlay();
-
 }//playSound
 
 
@@ -52,9 +44,15 @@ item.on("click",function() {
 });//playSound
 
 
-focusObj.addEventListener("click", function() {var buttonInnerHTML = this.innerHTML; eventMonitor(buttonInnerHTML);
+/*
+//play sound on mouse click
+item.on("click", function() {var buttonInnerHTML = this.innerHTML; eventMonitor(buttonInnerHTML);
 });
 
+//monitor the document & play sound on key entry
+  document.on("keydown", function(event) {
+  return eventMonitor(event.key);
+});
 
 /*
 
@@ -62,6 +60,7 @@ function eventTrigger(bindItem,trigger,monitorEvent,TriggerResponse) {
   var monitor = bindItem.on("monitorEvent",fubction() {
     bindItem.TriggerRespone(trigger)
 ;});
+
 
 var trigger = randomChosenColor;
 var item = $(`#${randomChosenColor}`);
